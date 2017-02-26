0cc91ed049bda7be9a4848666873b7e89c672b4c
Connor Bain <connorbain@u.northwestern.edu>
2017-02-26 17:55:52 -0600

    Initial BS Template
    Just grabs some recipe URL passed in as a String and extracts the three
    HTML sections we care about.

---
+++ recipe_transformer.py
@@ -0,0 +1,28 @@
+import sys
+from bs4 import BeautifulSoup
+import requests
+
+def main():
+    if len(sys.argv) != 2:
+        print "input error."
+        sys.exit(0)
+
+    recipeURL = str(sys.argv[1])
+
+    r  = requests.get(recipeURL)
+
+    data = r.text
+
+    soup = BeautifulSoup(data, "html.parser")
+
+    ingredientsHTML = soup.findAll("section", { "class" : "recipe-ingredients" })
+    print ingredientsHTML
+
+    directionsHTML = soup.findA ll("section", { "class" : "recipe-directions" })
+    print directionsHTML
+
+    footnotesHTML = soup.findAll("section", { "class" : "recipe-footnotes" })
+    print footnotesHTML
+
+if __name__ == "__main__":
+    main()
