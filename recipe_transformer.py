80c30c9cd7f6d24ef2e8477fb1f1695a88f37032
SamirJoshi <samirjoshi2018@u.northwestern.edu>
2017-02-28 13:11:24 -0600

    name/title and descrption added

--- recipe_transformer.py
+++ recipe_transformer.py
@@ -9,27 +9,27 @@ def main(recipeURL):
     data = r.text
 
     soup = BeautifulSoup(data, "html.parser")
+    ##GET TITLE AND DESCRIPTION
+    title = soup.findAll("h1", { "class" : "recipe-summary__h1" })[0].string
+    print "Name: ", title
+    descript = soup.findAll("div", { "class" : "submitter__description" })[0].contents[0]
+    print "Description: ", descript
+    ##GET THE INGREDIENTS
     ingredientsHTML = soup.findAll("li", { "class" : "checkList__line" })
-    #print "INGREDIENTS:", str(len(ingredientsHTML)), " , thing: ", str(ingredientsHTML[0].contents)
-
     ingredientsList = []
     for i in range(0, len(ingredientsHTML)):
-    #    print ingredientsHTML[i]
         ingredientsList.append(ingredientsHTML[i].findAll("span", { "class" : "recipe-ingred_txt"}))
-    #print "LIST: ", str(ingredientsList)
     final_ing_list = []
     for i in range(0, len(ingredientsList)):
         final_ing_list.append(ingredientsList[i][0].string)
 
-    #print "FINAL LIST: ", str(final_ing_list)
     types_of_measurement_list = ['teaspoon', 'cup', 'pound', 'dash', 'pinch',  'pint', 'quart', 'gallon', 'oz', 'oz.', 'liter', 'gram', 'ml', 'ounce', 'stick']
-    ing_dict = {}
     ing_list = []
     for i in range(0, len(final_ing_list)):
         split_ing = final_ing_list[0].split()
         split_ind = 0
         for j in range(0, len(split_ing)):
-            if any(keyword in split_ing[j] for keyword in types_of_measurement_list): 
+            if any(keyword in split_ing[j] for keyword in types_of_measurement_list):
                 split_ind = j
         ing_quant = split_ing[0:split_ind + 1]
         ing_quant = ' '.join(ing_quant)
@@ -47,4 +47,4 @@ def main(recipeURL):
 
 if __name__ == "__main__":
     recipe_url = raw_input("Please enter the URL of the recipe:")
-    main(recipe_url) 
+    main(recipe_url)
